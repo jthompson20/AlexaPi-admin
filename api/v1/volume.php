f82da0e418563a5441857448bab5bbe7c6f96540
Matt Thompson <matt.thompson@stack.com>
2016-12-03 11:36:07 -0500

    updated volume api

--- api/v1/volume.php
+++ api/v1/volume.php
@@ -1,6 +1,30 @@
 <?php
 // initialize variables
-$volume 	= (isset($_REQUEST['vol']))? $_REQUEST['vol']: '50';
-$command 	= 'amixer set Master '.$volume.'%';
+$method	 	= (isset($_REQUEST['method']))? $_REQUEST['method']: 'set';
 
-echo exec($command);
+// grab current volume
+$volume 	= json_decode(file_get_contents("data/volume.json"),TRUE);
+$volume 	= $volume['volume'];
+
+switch($method):
+	// ?method=set&volume=X
+	case "set":
+		// init vars
+		$volume 	= (isset($_REQUEST['volume']))? $_REQUEST['volume']: '50';
+		break;
+	// ?method=up
+	case "up":
+		$volume	= ($volume < 100)? $volume + 10: 100;
+		break;
+	// ?method=down
+	case "down":
+		$volume	= ($volume > 0)? $volume - 10: 0;
+		break;
+endswitch;
+
+// run the command
+$command 	= 'amixer set Master '.$volume.'%';
+exec($command);
+
+// input new volume into file
+file_put_contents('data/volume.json',json_encode(array('volume' => $volume)));
