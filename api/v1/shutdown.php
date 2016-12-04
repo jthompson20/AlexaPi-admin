874c0c4901401a184ed2a936730fb111d1e64b0c
Matt Thompson <matt.thompson@stack.com>
2016-12-03 21:05:59 -0500

    updated shutdown

--- api/v1/shutdown.php
+++ api/v1/shutdown.php
@@ -4,7 +4,7 @@
 $trigger 	= (isset($_REQUEST['type']))? $_REQUEST['type']: 'shutdown';
 
 // set command based on type
-$command 	= ($trigger == 'reboot')? 'sudo -u pi reboot': 'sudo -u pi shutdown';
+$command 	= ($trigger == 'reboot')? 'sudo -u root reboot': 'sudo -u root shutdown';
 
 // say goodbye message
 
